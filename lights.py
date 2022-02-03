e0e084f6fda9d6082367799ede84e432b64a7883
Shad <shad@chaz.pro>
2022-02-04 00:05:08 +0100

    :bulb: added lights control

--- lights.py
+++ lights.py
@@ -1,11 +1,23 @@
-from threading import Thread
+from datetime import time
 
-class LightsThread(Thread):
+from flask import g
+from gpiozero import TimeOfDay, PWLED
+
+class Lights:
     def __init__(self):
-        pass
+        self.daytime = TimeOfDay(time(8), time(20))
+        self.daytime.when_activated = self.onActivate
+        self.daytime.when_deactivated = self.onDeactivate
+        self.red = PWLED(16)
+        self.green = PWLED(20)
+        self.blue = PWLED(21)
 
-    def run(self):
-        self.running = True
-        while self.running:
-            pass
-        pass
+    def onActivate(self):
+        self.r.value = 1
+        self.g.value = 1
+        self.b.value = 1
+
+    def onDeactivate(self):
+        self.r.value = 0
+        self.g.value = 0
+        self.b.value = 0
